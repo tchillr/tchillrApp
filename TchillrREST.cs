9841195db82de12f31ca52564868b9a2eaaa3a45
tchillr <tchillr@gmail.com>
2013-06-02 21:31:08 +0200

    test

--- TchillrREST.cs
+++ TchillrREST.cs
@@ -20,37 +20,51 @@ namespace TchillrREST
     {
         public List<Data.Activity> GetAllActivities()
         {
+            
             List<Data.Activity> activities = new List<Data.Activity>();
 
-            WebRequest req = WebRequest.Create(@"https://api.paris.fr:3000/data/1.0/QueFaire/get_activities/?token=30539e0d4d810782e992a154e4dfa37bedb33652c6baf3fcbf7e6fd431482b23bbd8f892318ac3b58c45527e7aba721d&offset=0&limit=100");
-
-            req.Method = "GET";
-
-
-            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
-            if (resp.StatusCode == HttpStatusCode.OK)
+            try
             {
-                using (Stream respStream = resp.GetResponseStream())
-                {
-                    StreamReader reader = new StreamReader(respStream, Encoding.UTF8);
-                    JObject jsonActivities = JObject.Parse(reader.ReadToEnd());
-                    foreach (JObject activity in jsonActivities["data"])
-                    {
-                        Data.Activity act = new Data.Activity();
-                        act.Adresse = activity["adresse"].ToString();
-                        act.City = activity["city"].ToString();
-                        act.Description = activity["description"].ToString();
-                        act.Idactivites = (int)activity["idactivites"];
+                WebRequest req = WebRequest.Create(@"https://api.paris.fr:3000/data/1.0/QueFaire/get_activities/?token=30539e0d4d810782e992a154e4dfa37bedb33652c6baf3fcbf7e6fd431482b23bbd8f892318ac3b58c45527e7aba721d&offset=0&limit=100");
 
-                        activities.Add(act);
+                req.Method = "GET";
+
+
+                HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+                if (resp.StatusCode == HttpStatusCode.OK)
+                {
+                    using (Stream respStream = resp.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(respStream, Encoding.UTF8);
+                        JObject jsonActivities = JObject.Parse(reader.ReadToEnd());
+                        foreach (JObject activity in jsonActivities["data"])
+                        {
+                            Data.Activity act = new Data.Activity();
+                            act.Adresse = activity["adresse"].ToString();
+                            act.City = activity["city"].ToString();
+                            act.Description = activity["description"].ToString();
+                            act.Idactivites = (int)activity["idactivites"];
+
+                            activities.Add(act);
+                        }
                     }
                 }
+                else
+                {
+
+                }
             }
-            else
+            catch (Exception exp)
             {
-                
+                Data.Activity dumb = new Data.Activity();
+
+                dumb.Adresse ="dumb addr";
+                dumb.City = "dumb city";
+                dumb.Description = "dumb desc";
+                dumb.Idactivites = 1;
+
+                activities.Add(dumb);
             }
-           
 
             return activities;
         }
